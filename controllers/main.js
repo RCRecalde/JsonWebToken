bce5bf147a32f045aad3d8fcf2ffa9e832e6e751
RCRecalde <rorecalde3@gmail.com>
2023-02-27 15:46:39 -0300

    exercise token

---
+++ controllers/main.js
@@ -0,0 +1,16 @@
+const CustomAPIError = require('../errors/custom-error')
+const login = async (req, res) => {
+    const {username, password} = req.body
+    if(!username || !password){
+        throw new CustomAPIError('Porvide information', 400)
+    }
+res.send('Fake login/register/singup')
+    
+}
+const dashboard = async (req, res) => {
+    const luckyNumber = Math.floor(Math.random()*100)
+    res.status(200).json({msg: `Hello, John Doe`, secret: `Here is your authorized data, your lucky number kis ${luckyNumber}`})
+}
+
+module.exports = {login,dashboard
+}
